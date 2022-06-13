b92ee0132a09aefeb5a29e28941f3449764d3d59
jean viven <jeanvivien.sicot@lapiscine.pro>
2022-06-13 14:43:35 +0200

    fin exo

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once dirname(__DIR__, 2) . "/lib/Controller/AbstractController.php";
+
+class ContactController extends AbstractController
+{
+    public function index()
+    {
+        return $this->renderView(
+            "/template/contact/contact_base.phtml",
+            [
+                "name_page" => "contactPage",
+                "message" => "hello world"
+            ]
+        );
+    }
+}
